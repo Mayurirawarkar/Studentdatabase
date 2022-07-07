57723c3bf450a63322538d2d5d9d1a05a84e494d
manisharawarkar <108732374+Mayurirawarkar@users.noreply.github.com>
2022-07-07 18:53:58 +0530

    MyFirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { InsertComponent } from './insert/insert.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { StudentService } from './student.service';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
+import { ViewComponent } from './view/view.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InsertComponent,
+    UpdateComponent,
+    DeleteComponent,
+    ViewComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
